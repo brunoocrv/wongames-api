d94a0ab2f25fe8ca05c8d093aeccfe530f5c9364
Bruno Carvalho <brn.carvalho@icloud.com>
2023-11-01 20:14:15 -0300

    feat: adding scrapper and migrations

---
+++ src/api/game/controllers/game.ts
@@ -0,0 +1,21 @@
+/**
+ * game controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::game.game', ({ strapi }) => ({
+  async populate(ctx) {
+    console.log("Start to populate...");
+
+    const options = {
+      limit: 48,
+      order: "desc:trending",
+      ...ctx.query
+    }
+
+    await strapi.service('api::game.game').populate(options);
+
+    ctx.send("Finishing populate!!");
+  }
+}));
